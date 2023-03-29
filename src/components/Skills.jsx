fbdfdfcadea5abacf24c4708b9c0f600668f63bb
pushpraj paroha <satyamparoha786@gmail.com>
2023-03-30 02:03:18 +0530

    commits

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,7 +9,7 @@ import graphql from "../assets/graphql.png";
 import github from "../assets/github.png";
 import tailwind from "../assets/tailwind.png";
 
-const Experience = () => {
+const Skills = () => {
   const techs = [
     {
       id: 1,
@@ -64,7 +64,7 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen"
+      className="bg-gradient-to-b from-gray-800 to-black w-full h-full py-12"
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
@@ -90,4 +90,4 @@ const Experience = () => {
   );
 };
 
-export default Experience;
+export default Skills;

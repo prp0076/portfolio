94250d55cd3aca82ddda19853576a41d74873e9b
pushpraj paroha <satyamparoha786@gmail.com>
2023-04-01 01:10:14 +0530

    updates

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,37 +1,54 @@
 import React from "react";
-import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
-import installNode from "../assets/portfolio/installNode.jpg";
-import navbar from "../assets/portfolio/navbar.jpg";
-import reactParallax from "../assets/portfolio/reactParallax.jpg";
-import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
-import reactWeather from "../assets/portfolio/reactWeather.jpg";
+import dstore from "../assets/portfolio/dstore.jpg";
+// import installNode from "../assets/portfolio/installNode.jpg";
+import Menswear from "../assets/portfolio/menswear.jpg";
+import Movix from "../assets/portfolio/movix.jpg";
+// import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
+// import reactWeather from "../assets/portfolio/reactWeather.jpg";
 
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
-      src: arrayDestruct,
+      src: dstore,
+      href:"https://tangerine-fox-825b79.netlify.app/",
+      code:"https://github.com/prp0076/Dstore-Ecommerce"
     },
     {
       id: 2,
-      src: reactParallax,
+      src: Movix,
+      href:"https://thunderous-cascaron-53e154.netlify.app/",
+      code:"https://github.com/prp0076/MovieX"
+
     },
     {
       id: 3,
-      src: navbar,
+      src: Menswear,
+      href:"https://prp0076.github.io/Menswear/",
+      code:"https://github.com/prp0076/Menswear"
+
     },
-    {
-      id: 4,
-      src: reactSmooth,
-    },
-    {
-      id: 5,
-      src: installNode,
-    },
-    {
-      id: 6,
-      src: reactWeather,
-    },
+    // {
+    //   id: 4,
+    //   src: reactSmooth,
+    //   href:"https://tangerine-fox-825b79.netlify.app/",
+    //   code:"https://github.com/prp0076/Dstore-Ecommerce"
+
+    // },
+    // {
+    //   id: 5,
+    //   src: installNode,
+    //   href:"https://tangerine-fox-825b79.netlify.app/",
+    //   code:"https://github.com/prp0076/Dstore-Ecommerce"
+
+    // },
+    // {
+    //   id: 6,
+    //   src: reactWeather,
+    //   href:"https://tangerine-fox-825b79.netlify.app/",
+    //   code:"https://github.com/prp0076/Dstore-Ecommerce"
+
+    // },
   ];
 
   return (
@@ -48,7 +65,7 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src,href,code }) => (
             <div key={id} className=" shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -56,12 +73,15 @@ const Portfolio = () => {
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
-                <button className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                {/* <button className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
                   Demo
-                </button>
-                <button className="px-6 py-3 w-1/2 m-4 duration-200 hover:scale-105">
+                </button> */}
+                <a href={href} className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</a>
+                {/* <button className="px-6 py-3 w-1/2 m-4 duration-200 hover:scale-105">
                   Code
-                </button>
+                </button> */}
+                <a href={code} className=" px-6 py-3 w-1/2 m-4 duration-200 hover:scale-105">Code</a>
+
               </div>
             </div>
           ))}
